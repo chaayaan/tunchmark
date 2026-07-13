611ddb96459ae40d2b12b46f43a7496305aa80cc
Chayan <debchayan6@gmail.com>
2026-07-13 20:50:50 +0600

    reports are retrive and hallmark report updated

--- create_hallmark_report.php
+++ create_hallmark_report.php
@@ -661,8 +661,8 @@ if (isset($_GET['report_id'])) {
                 </div>
 
             </div><!-- /bottom 2-col -->
-        <div style="text-align:center;font-size:11px;color:#374151;margin:6px 0;color:#D97706;">
-            To check the authenticity of this report, scan the QR code or visit <b>www.rajaiswari.com</b> and enter HUID <b><?= htmlspecialchars($report_data['huid'] ?? '') ?></b> to verify this report.
+        <div style="text-align:center;font-size:11px;color:#006400;margin:6px 0;;">
+            To check the authenticity of this hallmark, scan the QR code or visit <b>www.rajaiswari.com</b> and enter HUID <b><?= htmlspecialchars($report_data['huid'] ?? '') ?></b> to verify this hallmark.
         </div>
 
         </div><!-- /reportPreview -->      
